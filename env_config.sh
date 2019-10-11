35d4270345c111df0afbb59ea723bb6072ff030b
tylerjgabb <tylerjgabb@gmail.com>
2019-10-11 19:20:14 -0400

    app factory and env config

---
+++ env_config.sh
@@ -0,0 +1,5 @@
+#this file shows the steps necessary to configure
+#the environment in which the flask app will run.
+#execute these commands before executing `flask run`
+export FLASK_APP=flaskr
+export FLASK_ENV=development
